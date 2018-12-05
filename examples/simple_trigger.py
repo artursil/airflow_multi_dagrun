4b941c493ee0d48bf1f574b8a29f1d5bfeb0a4d3
Alexander Seeholzer <seeholzer@gmail.com>
2018-12-05 18:27:38 +0100

    corrected indent

--- examples/simple_trigger.py
+++ examples/simple_trigger.py
@@ -1,31 +1,31 @@
-    from airflow.operators.dagrun_operator import DagRunOrder
-    from airflow.models import DAG
-    from airflow.operators import TriggerMultiDagRunOperator
-    from airflow.utils.dates import days_ago
+from airflow.operators.dagrun_operator import DagRunOrder
+from airflow.models import DAG
+from airflow.operators import TriggerMultiDagRunOperator
+from airflow.utils.dates import days_ago
 
 
-    def generate_dag_run(**kwargs):
-        for i in range(10):
-            order = DagRunOrder(payload={'my_variable': i})
-            yield order
+def generate_dag_run(**kwargs):
+    for i in range(10):
+        order = DagRunOrder(payload={'my_variable': i})
+        yield order
 
-    args = {
-        'start_date': days_ago(1),
-        'owner': 'airflow',
-    }
-
-
-    dag = DAG(
-        dag_id='simple_trigger',
-        max_active_runs=1,
-        schedule_interval='@hourly',
-        default_args=args,
-    )
+args = {
+    'start_date': days_ago(1),
+    'owner': 'airflow',
+}
 
 
-    gen_target_dag_run = TriggerMultiDagRunOperator(
-        task_id='gen_target_dag_run',
-        dag=dag,
-        trigger_dag_id='common_target',
-        python_callable=generate_dag_run
-    )
+dag = DAG(
+    dag_id='simple_trigger',
+    max_active_runs=1,
+    schedule_interval='@hourly',
+    default_args=args,
+)
+
+
+gen_target_dag_run = TriggerMultiDagRunOperator(
+    task_id='gen_target_dag_run',
+    dag=dag,
+    trigger_dag_id='common_target',
+    python_callable=generate_dag_run
+)
